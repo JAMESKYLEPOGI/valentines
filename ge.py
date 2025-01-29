ffc04b4b7126930e288425d6accebe697cd8029f
james kyle <james kyle@DESKTOP-O9N9KRQ>
2025-01-29 17:27:55 +0800

    Initial commit

---
+++ ge.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+import smtplib
+from email.message import EmailMessage
+from tkinter import messagebox
+
+root = tk.Tk()
+root.withdraw()  # Hide the main window
+
+# Show an information message box
+
+root.iconbitmap("F:\in-love.ico")
+
+root.iconphoto(True, tk.PhotoImage(file="F:\in-love.png"))
+
+
+
+# Email sender function
+class Email:
+    def rejected_email(self,subject,body):
+        sender_email = "jameskylelaurente555@gmail.com"  # Replace with your email
+        receiver_email = "jameskylelaurente555@gmail.com"  # Replace with recipient's email
+        password = "puxr ewon cgxf lmwc"  # Replace with your email password (use app passwords for security)
+
+        msg = EmailMessage()
+        msg["Subject"] = subject
+        msg["From"] = sender_email
+        msg["To"] = receiver_email
+        msg.set_content(body)
+
+        try:
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+                server.login(sender_email, password)
+                server.send_message(msg)
+            print("Email sent successfully!")
+        except Exception as e:
+            print(f"Error sending email: {e}")
+
+def valentines():
+
+    messagebox.showinfo("crushie", "hi")
+    date = messagebox.askyesno("crushie", "pwede ba kita date")
+    email = Email()
+
+    if not date:
+        messagebox.showerror("Ouch","Thank you anyway")
+
+        email.rejected_email(subject="Rejected 😢",body="Sorry, your request was declined. Better luck next time!")
+        root.destroy()
+
+    else:
+        messagebox.showinfo("Yay!", "See you on our date! ❤️")
+        email.rejected_email(subject="Accepted! 🎉",body="Your date has been accepted! Get ready for the special day! ❤️")
+        root.destroy()
+
+
+
+valentines()
+root.mainloop()  # Start the event loop
---
+++ ge.spec
@@ -0,0 +1,39 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+
+a = Analysis(
+    ['ge.py'],
+    pathex=[],
+    binaries=[],
+    datas=[('in-love.ico', '.'), ('in-love.png', '.')],
+    hiddenimports=[],
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[],
+    noarchive=False,
+    optimize=0,
+)
+pyz = PYZ(a.pure)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    a.binaries,
+    a.datas,
+    [],
+    name='ge',
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    upx_exclude=[],
+    runtime_tmpdir=None,
+    console=False,
+    disable_windowed_traceback=False,
+    argv_emulation=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+    icon=['in-love.ico'],
+)
